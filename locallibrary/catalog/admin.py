8002d725c87c43725ce95722801ed64cd28d0cfa
Даниэл Агаев <gr402_agdse@lab-402-1.tomtit-tomsk.ru>
2022-09-12 12:59:50 +0700

    add books

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -2,8 +2,30 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    @staticmethod
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
