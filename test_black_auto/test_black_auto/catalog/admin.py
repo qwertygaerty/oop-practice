40de8820608fefd428959a51b9a7f401c48878df
Даниэл Агаев <gr402_agdse@lab-402-1.tomtit-tomsk.ru>
2022-09-16 18:09:06 +0700

    create some models

--- test_black_auto/test_black_auto/catalog/admin.py
+++ test_black_auto/test_black_auto/catalog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from .models import Brand, CarModel, CarMalfunction, Malfunction
+
+
 # Register your models here.
+
+class AdminBrand(admin.ModelAdmin):
+    list_display = ('title', 'logo',)
+
+    pass
+
+
+class AdminCarModel(admin.ModelAdmin):
+    pass
+
+
+class AdminCarMalfunction(admin.ModelAdmin):
+    pass
+
+
+class AdminMalfunction(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Brand, AdminBrand)
+admin.site.register(CarModel, AdminCarModel)
+admin.site.register(CarMalfunction, AdminCarMalfunction)
+admin.site.register(Malfunction, AdminMalfunction)
